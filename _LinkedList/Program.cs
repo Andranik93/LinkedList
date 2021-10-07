40f6623c6edf19323767fa1d8610d428ea12ee9e
andranik.mesropyan <andranik.mesropyan@softconstruct.com>
2021-10-07 11:51:17 +0400

    first update

--- _LinkedList/Program.cs
+++ _LinkedList/Program.cs
@@ -10,11 +10,15 @@ namespace _LinkedList
             list.AddFirst(7);
             list.AddFirst(6);
             list.AddFirst(5);
+            list.AddFirst(50);
             list.AddFirst(4);
             list.AddLast(8);
+            list.AddLast(58);
             list.AddLast(9);
+            list.AddLast(19);
             list.Show(); 
             
+            // Remove functionality check
             list.RemoveFirst();
             list.RemoveLast();
 
